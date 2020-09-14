a3b5d2d91775663f4d2ac87c2ca208a9dd2708fb
pipat2468 <pipat_246@hotmail.com>
2020-09-14 19:42:59 +0700

    Fix plugin

---
+++ components/loading.vue
@@ -0,0 +1,29 @@
+<template>
+    <transition :name="animation">
+        <div class="loading-mask" v-if="num">
+            <slot/>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        animation: String
+    },
+    data() {
+        return {
+            num: 0
+        }
+    },
+    methods: {
+        show(bool) {
+            if (bool) {
+                this.num++
+            } else {
+                this.num--
+            }
+        }
+    }
+}
+</script>
